5e3e025fa1af0e3adac2c02a30591115fae696ad
Joe Walker <joewalk102@gmail.com>
2026-02-08 20:27:51 -0800

    Modify Dockerfile to build correctly. Also add in exception handling for skipping cred check during build.

--- src/TimeTrack/TimeTrack/config.py
+++ src/TimeTrack/TimeTrack/config.py
@@ -1,10 +1,20 @@
+from logging import getLogger
 from os import environ
+
+from google.auth.exceptions import DefaultCredentialsError
 
 from library.gcloud.sec_mgr import SecretManagerClient
 
-smc = SecretManagerClient(project_id=environ.get("GCP_PROJECT_ID"))
-loaded_vars = smc.load_secret_to_env(environ.get("GCP_SECRET_NAME"))
+log = getLogger(__name__)
 
+try:
+    smc = SecretManagerClient(project_id=environ.get("GCP_PROJECT_ID"))
+    loaded_vars = smc.load_secret_to_env(environ.get("GCP_SECRET_NAME"))
+except DefaultCredentialsError:
+    log.warning(
+        "Could not load secrets from Secret Manager. Using environment "
+        "variables instead."
+    )
 secret_key = environ.get("SECRET_KEY")
 allowed_hosts = environ.get("ALLOWED_HOSTS", "*").split(",")
 debug = environ.get("DEBUG", "True").lower() == "true"
